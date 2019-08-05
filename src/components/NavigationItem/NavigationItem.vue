06b85ece9d913e812fc81af96f6d6e1144595391
Tarun Gorai <goraitarunkumar@gmail.com>
2019-08-05 12:07:52 +0530

    default value fixed for array props

--- src/components/NavigationItem/NavigationItem.vue
+++ src/components/NavigationItem/NavigationItem.vue
@@ -48,7 +48,9 @@ export default {
      },
      selectedKeys: {
         type: Array,
-        default: [],
+        default: function () {
+          return []
+        },
      },
   },
   methods: {
